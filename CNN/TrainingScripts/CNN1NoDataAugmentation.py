259c99a91589ee0cc0d2df5f525a9457d56f482d
Jokubas007 <jokubasputrius@gmail.com>
2020-11-11 20:21:36 +0200

    Adding CNN models, logs, code

---
+++ CNN/TrainingScripts/CNN1NoDataAugmentation.py
@@ -0,0 +1,46 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Input, Flatten, Conv2D, BatchNormalization, \
+                                    Activation, Dropout, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import TensorBoard
+from IPython.display import YouTubeVideo
+from PIL import Image
+import pickle, gzip
+import numpy as np
+
+X, y = pickle.load(gzip.open('flatland_train.data', 'rb'))
+y[y != 0] -= 2
+X /= 255.
+X = X[..., np.newaxis]
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=[50, 50, 1]))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(5, activation='softmax'))
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(),
+              metrics=["accuracy"])
+
+root_logdir = os.path.join(os.curdir, "logs")
+
+def get_run_logdir():
+    run_id = 'CNN1_NoDataAugmentation'
+    return os.path.join(root_logdir, run_id)
+
+run_logdir = get_run_logdir()
+tensorboard_cb = TensorBoard(run_logdir)
+
+model.fit(X, y, epochs=20, batch_size=128, validation_split=0.2, callbacks=[tensorboard_cb])
+
+model.save('flatlandCNN1_NoDataAugmentation.h5')
